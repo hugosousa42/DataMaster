5aaba226c417efaf8f8141896b1738bea14cbbfa
Hugo Sousa <hugo.sousa@finsolutia.com>
2025-03-04 18:22:27 +0000

    Adding MVC project to repo.

---
+++ DataMaster/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DataMaster.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
