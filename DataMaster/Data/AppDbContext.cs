e70b1ca4555072bbfd9f3f83b7003a8b10699fe4
Hugo Sousa <hugo.sousa@finsolutia.com>
2025-03-04 18:53:08 +0000

    Product entity.

---
+++ DataMaster/Data/AppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using DataMaster.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace DataMaster.Data
+{
+    public class AppDbContext : DbContext 
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ DataMaster/Data/Entities/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace DataMaster.Data.Entities
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+
+        public string? Name { get; set; }
+    }
+}
